8f8b58b731b725943f5e8d0d3158b694b6224e2a
Andcool-Systems <drolll2007@gmail.com>
2026-07-08 03:10:10 +0300

    refactor: Refactor standard mathematical functions to use FFI with extern "C" and update module configuration for uv_sin

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,31 @@
 /// Standard Ultraviolet library
 
 /// STD sin function
-pub fn sin(x: f64) -> f64 {
+#[unsafe(no_mangle)]
+pub extern "C" fn uv_sin(x: f64) -> f64 {
     x.sin()
 }
 
 /// STD cos function
-pub fn cos(x: f64) -> f64 {
+#[unsafe(no_mangle)]
+pub extern "C" fn uv_cos(x: f64) -> f64 {
     x.cos()
 }
 
 /// STD tan function
-pub fn tan(x: f64) -> f64 {
+#[unsafe(no_mangle)]
+pub extern "C" fn uv_tan(x: f64) -> f64 {
     x.tan()
 }
 
 /// STD cot function
-pub fn cot(x: f64) -> f64 {
+#[unsafe(no_mangle)]
+pub extern "C" fn uv_cot(x: f64) -> f64 {
     1.0 / x.tan()
 }
 
 /// STD power function
-pub fn pow(x: f64, n: f64) -> f64 {
+#[unsafe(no_mangle)]
+pub extern "C" fn uv_pow(x: f64, n: f64) -> f64 {
     x.powf(n)
 }
